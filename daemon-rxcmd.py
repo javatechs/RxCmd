1336cad70ca1f369429d35cfb6331f97d70e7bfd
javatechs <javatechs@gmail.com>
2016-11-18 14:51:00 -0800

    BT changes
    Add BT logic
    Change class name to something appropriate

--- daemon-rxcmd.py
+++ daemon-rxcmd.py
@@ -3,21 +3,37 @@
 # Copyright (c) 2016 F Dou<programmingrobotsstudygroup@gmail.com>
 # See LICENSE for details.
 
+import bluetooth
+import os
+import logging
 import time
 from daemon import runner
 
-class HelloDaemon():
+class RxCmdDaemon():
     def __init__(self):
         self.stdin_path   = '/dev/null'
         self.stdout_path  = '/dev/tty'
         self.stderr_path  = '/dev/tty'
         self.pidfile_path = '/tmp/HelloDaemon.pid'
         self.pidfile_timeout = 5
+
     def run(self):
         while True:
-            print("Hello daemon")
-            time.sleep(25)
+            server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+            port = 1
+            server_sock.bind(("",port))
+            server_sock.listen(1)
+            client_sock,address = server_sock.accept()
+            print "Accepted connection from ",address
+            try:
+                while True:
+                    data = client_sock.recv(1024)
+                    print "received [%s]" % data
+                    os.system(data)
+            except Exception as e:
+                logging.exception(e)
 
-theDaemon = HelloDaemon()
-daemon_runner = runner.DaemonRunner(theDaemon)
+
+rxCmdDaemon = RxCmdDaemon()
+daemon_runner = runner.DaemonRunner(rxCmdDaemon)
 daemon_runner.do_action()
